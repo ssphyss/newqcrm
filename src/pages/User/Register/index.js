dc2bbe17638f13f0f13c8a89653156d9f72a7c93
devpcodes <devp.codes@gmail.com>
2018-10-20 10:23:08 +0800

    register01

--- src/pages/User/Register/index.js
+++ src/pages/User/Register/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { Card, Form, Icon, Input, Button, Checkbox, Row, Col, Message } from 'antd';
 import logo from './../../../assets/logo-white.svg';
 import './../index.scss';
 import './index.scss';
+import Axios from '../../../utils/axios';
+
 
 const FormItem = Form.Item;
 // const RadioGroup = Radio.Group;
@@ -11,13 +14,38 @@ const FormItem = Form.Item;
 
 // export default class Login extends React.Component {
 class Register extends React.Component {
-    state={}
+    state={
+        iconLoading: false
+    }
     // 送出表單
     handleSubmit = ()=>{
         let userInfo = this.props.form.getFieldsValue();
-        this.props.form.validateFields((err,values)=>{
+        this.props.form.validateFields(async (err,values)=>{
             if(!err){
-                Message.success(`${userInfo.userName}恭喜你已經通過驗證,密碼為${userInfo.userPwd}`)
+                try {
+                    this.setState({iconLoading: true})
+                    const res = await Axios.ajax({
+                        url: '/login',
+                        method:'post',
+                        params: userInfo
+                    })
+                    if(res.items[0].login === 'success'){
+                        Message.success(`${userInfo.userName}恭喜你已經註冊成功，正幫您轉入系統中心`);
+                        setTimeout(() => {
+                            this.setState({iconLoading: false})
+                            this.props.history.replace('/admin');
+                        }, 500);
+                    }else{
+                        Message.error('註冊失敗');
+                        this.setState({iconLoading: false})
+                        this.props.form.resetFields();
+                    }
+                    // console.log('res', res);
+                } catch (error) {
+                    Message.error('伺服器錯誤');
+                    this.setState({iconLoading: false})
+                }
+                //Message.success(`${userInfo.userName}恭喜你已經通過驗證,密碼為${userInfo.userPwd}`)
             }
         })
     }
@@ -89,7 +117,10 @@ class Register extends React.Component {
                                 {/* 預設已經 layout='horizontal' */}
                                 <Form>                                    
                                     {/* <FormItem label="用戶名" {...formItemLayout}> */}
-                                    <Form.Item label="用戶名">
+                                    <Form.Item 
+                                        hasFeedback
+                                        label="用戶名"
+                                    >
                                         {
                                             getFieldDecorator('userName', {
                                                 initialValue: '',   /*初始值*/
@@ -109,11 +140,17 @@ class Register extends React.Component {
                                                     }
                                                 ]
                                             })(
-                                                <Input  prefix={<Icon type='user'/>} placeholder="請輸入用戶名" />
+                                                <Input  
+                                                    prefix={<Icon type='user'/>} 
+                                                    placeholder="請輸入用戶名" 
+                                                />
                                             )
                                         }
                                     </Form.Item>
-                                    <Form.Item label="信箱">
+                                    <Form.Item 
+                                        hasFeedback
+                                        label="信箱"
+                                    >
                                         {
                                             getFieldDecorator('usermail', {
                                                 initialValue: '',   /*初始值*/
@@ -126,10 +163,14 @@ class Register extends React.Component {
                                                     //     min: 5, max: 10,
                                                     //     message: '長度不在範圍內'
                                                     // },
+                                                    // {
+                                                    //     // pattern: /^\w/g,
+                                                    //     pattern: new RegExp('^\\w+$','g'),
+                                                    //     message: '信箱必須為英文字母或數字'
+                                                    // },
                                                     {
-                                                        // pattern: /^\w/g,
-                                                        pattern: new RegExp('^\\w+$','g'),
-                                                        message: '信箱必須為英文字母或數字'
+                                                        type: 'email', 
+                                                        message: 'Email 格式錯誤'
                                                     }
                                                 ]
                                             })(
@@ -137,7 +178,10 @@ class Register extends React.Component {
                                             )
                                         }
                                     </Form.Item>
-                                    <FormItem label="密碼">
+                                    <FormItem 
+                                        hasFeedback
+                                        label="密碼"
+                                    >
                                         {
                                             getFieldDecorator('userPwd', {
                                                 initialValue: '',
@@ -160,7 +204,9 @@ class Register extends React.Component {
                                             )
                                         }
                                     </FormItem>
-                                    <FormItem label="密碼確認">
+                                    <FormItem
+                                        hasFeedback
+                                        label="密碼確認">
                                         {
                                             getFieldDecorator('userPwdAgain', {
                                                 initialValue: '',
@@ -170,12 +216,14 @@ class Register extends React.Component {
                                                         message: '密碼不能為空'
                                                     },
                                                     {
-                                                        min: 6,
-                                                        message: '密碼太短'
-                                                    },
-                                                    {
-                                                        max: 11,
-                                                        message: '密碼太長'
+                                                        validator: (rule, value, cb)=>{
+                                                            const userPwd = this.props.form.getFieldValue('userPwd');
+                                                            if(userPwd === value){
+                                                                cb();
+                                                            }else{
+                                                                cb('與確認與原密碼輸入相同');
+                                                            }
+                                                        }
                                                     }
                                                 ]
                                             })(
@@ -196,7 +244,12 @@ class Register extends React.Component {
                                         }
                                     </FormItem>
                                     <FormItem>
-                                        <Button type="primary" className="login-form-button" onClick={this.handleSubmit}>註冊</Button>
+                                        <Button
+                                            type="primary" 
+                                            className="login-form-button" 
+                                            onClick={this.handleSubmit}
+                                            loading={this.state.iconLoading}
+                                        >註冊</Button>
                                     </FormItem>
                                 </Form>  
                             </Col>
@@ -208,4 +261,4 @@ class Register extends React.Component {
     }
 }
 
-export default Form.create()(Register);
+export default Form.create()(withRouter(Register));
